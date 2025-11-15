0a23f9efb19e3bd58c3f273e776d566f029093f9
Ever Alonso Torres Galeano <eeatg844@hotmail.com>
2025-11-15 15:43:15 -0500

    Set Base LIne Page

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import Layout from './components/Layout/Layout';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
-import ApplicationsList from './pages/ApplicationsList';
 import CompaniesList from './pages/CompaniesList';
-import UsersList from './pages/UsersList';
 import ViewUser from './pages/ViewUser';
 import EditUser from './pages/EditUser';
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
@@ -32,9 +30,7 @@ const App: React.FC = () => {
               <Layout>
                 <Routes>
                   <Route path="/dashboard" element={<Dashboard />} />
-                  <Route path="/applications" element={<ApplicationsList />} />
                   <Route path="/companies" element={<CompaniesList />} />
-                  <Route path="/users" element={<UsersList />} />
                   <Route path="/user/view" element={<ViewUser />} />
                   <Route path="/user/edit" element={<EditUser />} />
                   <Route path="/" element={<Navigate to="/dashboard" replace />} />
